f8131ce556d5d5dccd2cc85bc1de1d40b3726c8f
GabeSpielmann <102567036+GabeSpielmann@users.noreply.github.com>
2022-03-28 23:10:30 -0300

    .

---
+++ TRAB/index.js
@@ -0,0 +1,94 @@
+
+const fields = document.querySelectorAll("[required]")
+
+
+function ValidateField(field) {
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            if (field.validity[error] && !field.validity.valid ) {
+                foundError = error
+            }
+        }
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                typeMismatch: "Por favor, preencha um email válido"
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+        
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = "red"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "green"
+            setCustomMessage()
+        }
+    }
+}
+
+const tel = document.getElementById('tel') 
+
+tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) 
+tel.addEventListener('change', (e) => mascaraTelefone(e.target.value)) 
+
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "")
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
+    tel.value = valor
+}
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for( field of fields ){
+    field.addEventListener("invalid", event => { 
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
+}
+
+
+document.querySelector("form")
+.addEventListener("submit", event => {
+    console.log("enviar o formulário")
+
+
+    event.preventDefault()
+})
